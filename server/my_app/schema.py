9983ed12facef55840984a54f96abf0ae385c99e
HitmanTheGreat <hitmantalmud@gmail.com>
2024-09-30 10:28:59 +0200

    fully working graph ql crud now left with user permissions and authentication using jwt tokens

--- server/my_app/schema.py
+++ server/my_app/schema.py
@@ -7,13 +7,66 @@ class CompanyType(DjangoObjectType):
     model = Company
     fields = ("id", "name", "address")
     
+
 class Query(graphene.ObjectType):
-  """
-  Queries for the Restaurant model
-  """
-  restaurants = graphene.List(CompanyType)
+    
+    companies = graphene.List(CompanyType)
+    get_company_by_id = graphene.Field(CompanyType, id=graphene.Int(required=True))
 
-  def resolve_restaurants(self, info, **kwargs):
-    return Company.objects.all()
+    def resolve_companies(self, info, **kwargs):
+        return Company.objects.all()
 
-schema = graphene.Schema(query=Query)
+    def resolve_get_company_by_id(self, info, id):
+        try:
+            return Company.objects.get(id=id)
+        except Company.DoesNotExist:
+            return None
+
+
+class CreateCompany(graphene.Mutation):
+    class Arguments:
+        name = graphene.String()
+        address = graphene.String()
+        
+    ok = graphene.Boolean() 
+    company = graphene.Field(CompanyType)
+
+    def mutate(self, info, name, address):
+        company = Company(name=name, address=address)
+        company.save()
+        return CreateCompany(ok=True, company=company)
+    
+class DeleteCompany(graphene.Mutation):
+  class Arguments:
+    id = graphene.Int()
+
+  ok = graphene.Boolean()
+  
+  def mutate(self, info, id):
+    restaurant = Company.objects.get(id=id)
+    restaurant.delete()
+    return DeleteCompany(ok=True)
+
+class UpdateCompany(graphene.Mutation):
+  class Arguments:
+    id = graphene.Int()
+    name = graphene.String()
+    address = graphene.String()
+
+  ok = graphene.Boolean()
+  company = graphene.Field(CompanyType)
+
+  def mutate(self, info, id, name, address):
+    company = Company.objects.get(id=id)
+    company.name = name
+    company.address = address
+    company.save()
+    return UpdateCompany(ok=True, company=company)
+
+    
+class Mutation(graphene.ObjectType):
+  create_company = CreateCompany.Field()
+  delete_company = DeleteCompany.Field()
+  update_company = UpdateCompany.Field()
+  
+schema = graphene.Schema(query=Query, mutation=Mutation)
